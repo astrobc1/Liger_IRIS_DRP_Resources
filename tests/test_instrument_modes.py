a9231f6134068d275473935a60a12df4c1a8323f
Bryson Cale <bryson.cale1@gmail.com>
2026-07-24 09:36:52 -0700

    Updated available gratings (17 -> 14) to reflct science priorities.

--- tests/test_instrument_modes.py
+++ tests/test_instrument_modes.py
@@ -2,9 +2,9 @@ from liger_iris_drp_resources import gratings, make_liger_modes_table, make_iris
 
 def test_instrument_modes():
     modes = make_liger_modes_table()
-    num_modes = 411
+    num_modes = 301
     assert len(modes) == num_modes, f"Expected {num_modes} modes for Liger, but got {len(modes)}."
 
     modes = make_iris_modes_table()
-    num_modes = 406
+    num_modes = 310
     assert len(modes) == num_modes, f"Expected {num_modes} modes for IRIS, but got {len(modes)}."
